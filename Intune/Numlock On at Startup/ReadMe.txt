27213aee6f6f8739c10c83bf74b7a9c04223d9e3
Michiel Matthys <micmat100@hotmail.com>
2026-06-23 11:51:33 +0200

    Numlcok on app

---
+++ Intune/Numlock On at Startup/ReadMe.txt
@@ -0,0 +1,15 @@
+Install command
+powershell.exe -ExecutionPolicy Bypass -File script.ps1
+
+Uninstall command
+cmd.exe /c exit 0
+
+Detection Rule
+Rules format
+Manually configure detection rules
+Detection rules
+Registry HKEY_USERS\.DEFAULT\Control Panel\Keyboard
+InitialKeyboardIndicators
+Detection method String comparison
+Equals
+2
---
+++ Intune/Numlock On at Startup/script.ps1
@@ -0,0 +1,11 @@
+﻿$path = "Registry::HKEY_USERS\.DEFAULT\Control Panel\Keyboard"
+$name = "InitialKeyboardIndicators"
+$value = "2"
+
+# Create path if it doesn't exist
+if (-not (Test-Path $path)) {
+    New-Item -Path $path -Force | Out-Null
+}
+
+# Set the value
+New-ItemProperty -Path $path -Name $name -Value $value -PropertyType String -Force | Out-Null
