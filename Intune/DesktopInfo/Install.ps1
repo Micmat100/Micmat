65f65c856a1ed69b58940a6c08522a0b044319ea
Michiel Matthys <micmat100@hotmail.com>
2026-06-23 10:24:54 +0200

    Added DesktopInfo files

---
+++ Intune/DesktopInfo/Install.ps1
@@ -0,0 +1,16 @@
+﻿$DestinationFolder = "C:\DesktopInfo"
+# Create folder
+if (!(Test-Path $DestinationFolder)) {
+   New-Item -Path $DestinationFolder -ItemType Directory -Force | Out-Null
+}
+# Get script source folder (Intune-safe)
+$source = $PSScriptRoot
+# Copy all files from package source
+Copy-Item -Path "$source\*" -Destination $DestinationFolder -Recurse -Force
+# Create startup shortcut
+$StartupFolder = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
+$WshShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$StartupFolder\DesktopInfo.lnk")
+$Shortcut.TargetPath = "$DestinationFolder\DesktopInfo.exe"
+$Shortcut.WorkingDirectory = $DestinationFolder
+$Shortcut.Save()
---
+++ Intune/DesktopInfo/Uninstall.ps1
@@ -0,0 +1,5 @@
+﻿Get-Process DesktopInfo -ErrorAction SilentlyContinue | Stop-Process -Force
+Remove-Item `
+ "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\DesktopInfo.lnk" `
+ -Force -ErrorAction SilentlyContinue
+Remove-Item "C:\DesktopInfo" -Recurse -Force -ErrorAction SilentlyContinue
